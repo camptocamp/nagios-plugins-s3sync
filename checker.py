0a3fcf9bb200e52a4a0fb50ea31a70d95c8cfb6b
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2018-01-15 09:25:38 +0100

    support 2 date formats in logs

--- checker.py
+++ checker.py
@@ -29,23 +29,39 @@ class checker:
         if self.__debug >= level:
             print string
 
+    def __matching(self, matchers, iteration):
+      now  = datetime.now()
+      year = now.year
+      if matchers:
+          self.__print('Matcher found for %s' % iteration)
+          for m in matchers:
+              date_str = '%i %s %s %s' % (year, m.group('month'), m.group('day').zfill(2), m.group('time'))
+              try:
+                last = datetime.strptime(date_str, '%Y %b %d %H:%M:%S')
+              except ValueError:
+                last = datetime.strptime(date_str, '%Y %m %d %H:%M:%S')
+              self.__print('Found: %s' % last)
+              if last.date() == now.date() or now.hour < self.__threshold and last.date() == (now - timedelta(days=1)).date():
+                  self.__print('Threshold is met')
+                  self.out_msg = 'OK: last s3 pull %s' % last
+                  self.out_status  = 0
+      else:
+        self.__print('No matcher found for %s' % iteration)
+
+
     def __find_log(self):
-        reg  = re.compile('(?P<month>[A-z]{3})\s+(?P<day>[0-9]{1,2}) (?P<time>[0-9]{2}:[0-9]{2}:[0-9]{2}) \w+ %s: success' % self.__title)
+        reg1 = re.compile('(?P<month>[A-z]{3})\s+(?P<day>[0-9]{1,2}) (?P<time>[0-9]{2}:[0-9]{2}:[0-9]{2}) \w+ %s: success' % self.__title)
+        reg2 = re.compile('(?P<year>[0-9]{4})-(?P<month>[0-9]{2})-(?P<day>[0-9]{2})T(?P<time>[0-9]{2}:[0-9]{2}:[0-9]{2}).+ %s: success' % self.__title)
         now  = datetime.now()
         year = now.year
         for log in self.__logs:
             self.__print('Testing %s' % os.path.join('/var/log', log))
             f = open(os.path.join('/var/log', log), 'r').read()
-            matchers = reg.findall(f)
-            if matchers and len(matchers) > 0:
-                for m in matchers:
-                    date_str = '%i %s %s %s' % (year, m[0], m[1].zfill(2), m[2])
-                    last = datetime.strptime(date_str, '%Y %b %d %H:%M:%S')
-                    self.__print('Found: %s' % last)
-                    if last.date() == now.date() or now.hour < self.__threshold and last.date() == (now - timedelta(days=1)).date():
-                        self.__print('Threshold is met')
-                        self.out_msg = 'OK: last s3 pull %s' % last
-                        self.out_status  = 0
+	    matchers1 = reg1.finditer(f)
+            self.__matching(matchers1, '1')
+            matchers2 = reg2.finditer(f)
+            self.__matching(matchers2, '2')
+
 
 
 
